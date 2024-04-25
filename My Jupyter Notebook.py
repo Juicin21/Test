4463d2564d22ebf95d11943d7b861cc1b5bee078
Juicin21 <168016239+Juicin21@users.noreply.github.com>
2024-04-25 08:13:00 -0400

    Add files via upload

---
+++ My Jupyter Notebook.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+#  **Max** 
+#  
+#  Project manager
+
+# _my interest in data science comes from a passion for making new discoveries and solving problems with data._
+
+# ### This code demonstrates the use of functions, recursion, and basic arithmetic operations in Python.
+
+# In[5]:
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Test the function
+number = 5
+print("The factorial of", number, "is", factorial(number))
+
+
+# # Here are some of the best places to eat in Massachusetts: 
+# 1. Bostonia Public House
+# 2. Prima
+# 3. Moo Steakhouse
+# 
+# # These are some sample grades for kids in Mrs. Jones' Class: 
+# | Student ID| Name | Math | Science | English |
+# | ----------| ---- | ---- | ------- | ------- |
+# | 1         | John | 85   | 90      | 88      |
+# | 2         | Alice | 92  | 88      | 95      | 
+# | 3         | Bob  | 78   | 85      | 80      | 
+# | 4         | Emily | 90  | 92      | 87      |
+# | 5         | Sophia | 85 | 80      | 90      | 
+# 
+# # This is a picture of a puppy 
+# ![Alt text](<a href="https://www.vecteezy.com/png/29472006-a-cute-pitbull-with-a-happy-smaile-ai-generative">a cute pitbull with a happy smile ai generative PNGs by Vecteezy</a>)
+# 
+
+# In[ ]:
+
+
+
+
